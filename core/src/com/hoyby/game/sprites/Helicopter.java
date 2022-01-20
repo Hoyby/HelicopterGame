44741a285e289f77af29ae7fd1c58aa088bc36ec
hoyby <alex.hoeyby@gmail.com>
2022-01-20 17:14:32 +0100

    added multiple enemies

--- core/src/com/hoyby/game/sprites/Helicopter.java
+++ core/src/com/hoyby/game/sprites/Helicopter.java
@@ -2,8 +2,8 @@ package com.hoyby.game.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector3;
-import com.hoyby.game.MyHeliGame;
 
+import java.util.HashMap;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -11,33 +11,15 @@ import java.util.TimerTask;
 public class Helicopter {
     private final Vector3 position;
     private final Vector3 velocity;
-
     private final Texture helicopter;
+    private final HashMap<String, Integer> bounds;
 
-    public Helicopter(int x, int y) {
+
+    public Helicopter(int x, int y, HashMap<String, Integer> bounds) {
         position = new Vector3(x, y, 0);
         velocity = new Vector3(0, 0, 0);
         helicopter = new Texture("helicopter/heli1.png");
-    }
-
-    public void update(float dt) {
-        if (position.y > 0) {
-            velocity.add(0, 0, 0);
-        }
-        velocity.scl(dt);
-        position.add(velocity.x, velocity.y, 0);
-        if (position.y < 0) {
-            velocity.y = Math.abs(velocity.y);
-        } else if (position.y > (MyHeliGame.HEIGHT) - helicopter.getHeight()) {
-            velocity.y = -Math.abs(velocity.y);
-        }
-        if (position.x < 0) {
-            velocity.x = Math.abs(velocity.x);
-        } else if (position.x > (MyHeliGame.WIDTH) - helicopter.getWidth()) {
-            velocity.x = -Math.abs(velocity.x);
-        }
-
-        velocity.scl(1 / dt);
+        this.bounds = bounds;
     }
 
     public Vector3 getPosition() {
@@ -52,16 +34,51 @@ public class Helicopter {
         return velocity;
     }
 
-    public void fly() {
+    private Boolean validateCurrentPos() {
+        if (position.y < bounds.get("minY")) {
+            velocity.y = Math.abs(velocity.y);
+            return false;
+        } else if (position.y > bounds.get("maxY") - helicopter.getHeight()) {
+            velocity.y = -Math.abs(velocity.y);
+            return false;
+        }
+        if (position.x < bounds.get("minX")) {
+            velocity.x = Math.abs(velocity.x);
+            return false;
+        } else if (position.x > bounds.get("maxX") - helicopter.getWidth()) {
+            velocity.x = -Math.abs(velocity.x);
+            return false;
+        }
+        return true;
+    }
+
+    public void update(float dt) {
+        if (position.y > 0) {
+            velocity.add(0, 0, 0);
+        }
+        velocity.scl(dt);
+        position.add(velocity.x, velocity.y, 0);
+        validateCurrentPos();
+
+        velocity.scl(1 / dt);
+    }
+
+    public void flyTowards(int x, int y) {
+        if (validateCurrentPos()) {
+            velocity.y = y - position.y;
+            velocity.x = x - position.x;
+        }
+    }
+
+    public void flyRandom() {
+        final int speed = 1000;
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 // ensures constant velocity in a random direction
-                velocity.x = new Random().nextInt(200) - 100;
-                velocity.y = new Random().nextBoolean() ? -(100 - (Math.abs(velocity.x))) : 100 - (Math.abs(velocity.x));
-
-
+                velocity.x = new Random().nextInt(speed) - speed / 2;
+                velocity.y = new Random().nextBoolean() ? -(speed / 2 - (Math.abs(velocity.x))) : speed / 2 - (Math.abs(velocity.x));
             }
         }, 0, 3 * 1000);
     }
--- core/src/com/hoyby/game/states/PlayState.java
+++ core/src/com/hoyby/game/states/PlayState.java
@@ -2,44 +2,78 @@ package com.hoyby.game.states;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.hoyby.game.MyHeliGame;
 import com.hoyby.game.sprites.Helicopter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
 
 public class PlayState extends State {
 
     private final Helicopter helicopter;
     private final Texture bg;
+    private final BitmapFont coords;
+    private final HashMap<String, Integer> bounds = new HashMap<>();
+    private final List<Helicopter> enemies = new ArrayList<>();
+
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
-        helicopter = new Helicopter(50, 300);
-        cam.setToOrtho(false, MyHeliGame.WIDTH, MyHeliGame.HEIGHT);
         bg = new Texture("bg.jpg");
-        helicopter.fly();
+        System.out.println(bg.getWidth() + " " + bg.getHeight());
+        cam.setToOrtho(false, bg.getWidth() / 2, bg.getHeight() / 2);
+        coords = new BitmapFont();
+        bounds.put("minX", -((bg.getWidth() / 2) - (MyHeliGame.WIDTH / 2)));
+        bounds.put("maxX", bounds.get("minX") + bg.getWidth());
+        bounds.put("minY", -((bg.getHeight() / 2) - (MyHeliGame.HEIGHT / 2)));
+        bounds.put("maxY", bounds.get("minY") + bg.getHeight());
+        helicopter = new Helicopter(50, 300, bounds);
+        for (int i = 0; i < 10; i++) {
+            enemies.add(new Helicopter(
+                    bounds.get("maxX") - new Random().nextInt(bg.getWidth()),
+                    bounds.get("maxY") - new Random().nextInt(bg.getHeight()),
+                    bounds));
+        }
+        for (Helicopter enemy : enemies) {
+            enemy.flyRandom();
+        }
     }
 
     @Override
     protected void handleInput() {
-        if (Gdx.input.justTouched()) {
-
+        if (Gdx.input.isTouched()) {
+            int x = bounds.get("minX") + (Gdx.input.getX() * (bg.getWidth() / MyHeliGame.WIDTH)) - (helicopter.getTexture().getWidth() / 2);
+            int y = bounds.get("maxY") - (Gdx.input.getY() * (bg.getHeight() / MyHeliGame.HEIGHT)) - (helicopter.getTexture().getHeight() / 2);
+            helicopter.flyTowards(x, y);
         }
     }
 
     @Override
     public void update(float dt) {
         handleInput();
+        for (Helicopter e : enemies) {
+            e.update(dt);
+        }
         helicopter.update(dt);
+        cam.position.set(helicopter.getPosition().x + (helicopter.getTexture().getWidth() / 2), helicopter.getPosition().y + (helicopter.getTexture().getHeight() / 2), 0);
+        cam.update();
     }
 
     @Override
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(bg, 0, 0);
+        sb.draw(bg, -((bg.getWidth() / 2) - (MyHeliGame.WIDTH / 2)), -((bg.getHeight() / 2) - (MyHeliGame.HEIGHT / 2)));
         sb.draw(helicopter.getTexture(), helicopter.getVelocity().x > 0 ? helicopter.getPosition().x + helicopter.getTexture().getWidth() : helicopter.getPosition().x, helicopter.getPosition().y, helicopter.getVelocity().x > 0 ? -helicopter.getTexture().getWidth() : helicopter.getTexture().getWidth(), helicopter.getTexture().getHeight());
+        for (Helicopter enemy : enemies) {
+            sb.draw(enemy.getTexture(), enemy.getVelocity().x > 0 ? enemy.getPosition().x + enemy.getTexture().getWidth() : enemy.getPosition().x, enemy.getPosition().y, enemy.getVelocity().x > 0 ? -enemy.getTexture().getWidth() : helicopter.getTexture().getWidth(), helicopter.getTexture().getHeight());
+        }
+        coords.draw(sb, "X: " + Math.round(helicopter.getPosition().x) + " Y: " + Math.round(helicopter.getPosition().y), cam.position.x - (cam.viewportWidth / 2) + 20, cam.position.y + (cam.viewportHeight / 2) - 20);
         sb.end();
-
     }
 
     @Override
